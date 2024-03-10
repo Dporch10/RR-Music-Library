c16959405c6f2403a93b6d221c9a15e13a85f77a
DPorch10 <aintshit@DPorch10s-MacBook-Pro.local>
2024-03-10 13:28:41 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,41 +1,64 @@
 import './App.css';
-import { useEffect, useState } from 'react'
+import { useEffect, useState,useRef, Fragment } from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import ArtistView from './components/ArtistView'
+import AlbumView from './components/AlbumView'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 
-function App() {
-  let [searchTerm, setSearchTerm] = useState('')
-  let [data, setData] = useState([])
-  let [message, setMessage] = useState('Search for Music!')
 
-  useEffect(() => {
-    if (searchTerm) {
-      document.title=`${searchTerm} Music`
-      const fetchData = async () => {
-        const response = await fetch(`https://itunes.apple.com/search?term=${searchTerm}`)
-        const resData = await response.json()
-        if(resData.results.length > 0) {
-          setData(resData.results)
-        } else {
-          setMessage('Not Found')
-        }
-      }
-      fetchData()
-  }
-  }, [searchTerm])
+function App(){
+  let searchInput = useRef('')
+  let [data, setData] = useState(null)
+  let [message, setMessage] = useState('Search for Music!')
 
   const handleSearch = (e, term) => {
     e.preventDefault()
-    setSearchTerm(term)
+    setData(fetchData(term, 'main'))
   }
+
+  const API_URL = 'https://itunes.apple.com/search?term='
+
+    useEffect(() => {
+      if (search) {
+      const fetchData = async () => {
+          document.title = `${search} Music`
+          const response = await fetch(API_URL + search)
+          const resData = await response.json()
+          if (resData.results.length > 0) {
+              setData(resData.results)
+          } else {
+              setMessage('Not Found')
+          }
+      }
+      fetchData()
+    }
+  }, [search])
+
+  
 
   return (
     <div className="App">
-      <SearchBar handleSearch={handleSearch} />
       {message}
-      <Gallery data={data} />
+      <Router>
+        <Route exact path={'/'}>
+          <SearchContext.Provider value={{term: searchInput, handleSearch: handleSearch}}>
+            <SearchBar />
+          </SearchContext.Provider>
+            <DataContext.Provider value={data}>
+              {renderGallery()}
+            </DataContext.Provider>
+        </Route>
+        <Route path="/album/:id">
+          <AlbumView />
+        </Route>
+        <Route path="/artist/:id">
+          <ArtistView />
+        </Route>
+      </Router>
     </div>
   );
 }
 
-export default App;
+
+export default App
